b6a124e96c1089f5b766780604f4e76c19567a02
radiaide <test@gmail.com>
2023-12-05 17:48:06 +0100

    regler le probleme du hashage du mot de pass

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <script src="./js/main.js" defer></script>
     <title>Sign In</title>
 </head>
+
 <body class="bg-gray-100 h-screen flex items-center justify-center">
 
     <div class="bg-white p-8 rounded w-96 shadow-md max-w-md rounded-2xl">
@@ -17,67 +19,70 @@
         require_once "./includes/config/connection.php";
         // Check if the form is submitted
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            // Get form data
-            $email = $_POST["email"];
-            $password = $_POST["password"];
+            $email = $_POST['email'];
+            $motDePasse = $_POST['password'];
 
-            // Query to check user credentials
-            $query = "SELECT pass_word, role FROM users WHERE email = ?";
-            $stmt = $conn->prepare($query);
+            $requete = $conn->prepare("SELECT * FROM users WHERE email = ?");
+            if (!$requete) {
+                die("Erreur de préparation de la requête : " . $conn->error);
+            }
 
-            if ($stmt) {
-                $stmt->bind_param("s", $email);
-                $stmt->execute();
-                $stmt->bind_result($hashedPassword, $role);
-                $stmt->fetch();
-                $stmt->close();
-                // Verify password
-                if (password_verify($password, $hashedPassword)) {
-                    // Password is correct, you can redirect the user to another page if needed
-                    // For now, let's just echo a success message
-                    // Assuming $role is the role retrieved from the database
-                    if ($role === 'scrum_master') {
-                        header("Location: ./scrum_master/src/index.php");
-                        exit();
-                    } elseif ($role === 'product_owner') {
-                        header("Location: ./product_owner/src/index.php");
-                        exit();
-                    } elseif ($role === 'user') {
+            $requete->bind_param("s", $email);
+            $requete->execute();
+            $resultat = $requete->get_result();
+
+            if ($utilisateur = $resultat->fetch_assoc()) {
+
+                if (password_verify($motDePasse, $utilisateur['pass_word'])) {
+                    session_start();
+                    $_SESSION['utilisateur'] = [
+                        'id' => $utilisateur['id'],
+                    ];
+                    if ($utilisateur['role'] == 'user') {
                         header("Location: ./Membre/src/index.php");
                         exit();
+                    } elseif ($utilisateur['role'] == 'po') {
+                        header("Location:  ./ProductOwner/src/index.php");
+                        exit();
                     } else {
-                        // Handle unknown role or other cases
-                        // You can redirect to a generic dashboard or display an error message
-                        header("Location: generic_dashboard.php");
+                        header("Location:  ./ScrumMaster/src/index.php");
                         exit();
                     }
-                    echo "<p class='text-green-500 text-center'>Login successful! Welcome, $username!</p>";
                 } else {
-                    // Incorrect password
-                    echo "<p class='text-red-500 text-center'>Incorrect email or password.</p>";
+                    echo "Mot de passe incorrect.";
                 }
             } else {
-                // Handle database connection error
-                echo "<p class='text-red-500 text-center'>Error: " . $con->error . "</p>";
+                echo "Aucun utilisateur trouvé avec cet email.";
             }
+
+            $requete->close();
         }
+
         ?>
-        <form name="signInForm" action="index.php" method="POST" onsubmit="return validateForm();">   
-            <!--Email input-->    
-            <div class="mb-4">  
-                <input type="email" id="email" name="email" placeholder="Email" class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500 drop-shadow-lg" required>
+        <form name="signInForm" action="index.php" method="POST" onsubmit="return validateForm();">
+            <!--Email input-->
+            <div class="mb-4">
+                <input type="email" id="email" name="email" placeholder="Email"
+                    class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500 drop-shadow-lg"
+                    required>
             </div>
-            <!--Password input-->   
+            <!--Password input-->
             <div class="mb-6">
                 <label for="password" class="sr-only">Password</label>
-                <input type="password" id="password" name="password" placeholder="Password" class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500 drop-shadow-lg" required>
+                <input type="password" id="password" name="password" placeholder="Password"
+                    class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500 drop-shadow-lg"
+                    required>
             </div>
 
-            <button type="submit" class="w-full bg-red-500 text-white py-2 rounded-2xl hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">Get started</button>
-            <p class="mt-4 text-gray-600 text-xs text-center">Don't have an account? <a href="./SignUp.php" class="text-blue-500 hover:underline">Sign up here</a>.</p>
+            <button type="submit"
+                class="w-full bg-red-500 text-white py-2 rounded-2xl hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">Get
+                started</button>
+            <p class="mt-4 text-gray-600 text-xs text-center">Don't have an account? <a href="./SignUp.php"
+                    class="text-blue-500 hover:underline">Sign up here</a>.</p>
         </form>
 
     </div>
 
 </body>
+
 </html>
