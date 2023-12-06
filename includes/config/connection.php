b02372f07fc5af18ed6b7c3cdc44ad82eada261c
ahmed-hounati <hounatiahmed@gmail.com>
2023-12-06 11:58:06 +0100

    recherche function

--- includes/config/connection.php
+++ includes/config/connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn=new mysqli("localhost","root","","dataware7");
+$conn=new mysqli("localhost","root","","dataware_db");
 if($conn->connect_error){
     die("connection failed : " . $connection->connect_error);}
 
