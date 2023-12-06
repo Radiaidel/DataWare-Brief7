c7f986ff8a28f892e700cedaf949d47eb16536bc
radiaide <test@gmail.com>
2023-12-06 15:53:29 +0100

    afficher les questions du projet clique + suggestions de tag v1

---
+++ Membre/src/action/tag_suggestions.php
@@ -0,0 +1,26 @@
+<?php
+include("../../../includes/config/connection.php");
+include '../../template/header.php';
+
+
+$prefix = $_POST['prefix'];
+$prefixModified = $prefix . "%";
+
+$sql = "SELECT id_tag,tag_name FROM tags WHERE tag_name LIKE ? LIMIT 5";
+$stmt = $conn->prepare($sql);
+
+if ($stmt === false) {
+    die("Erreur de préparation de la requête : " . $conn->error);
+}
+
+$stmt->bind_param("s", $prefixModified);
+$stmt->execute();
+$result = $stmt->get_result();
+
+while ($row = $result->fetch_assoc()) {
+    echo  "<option value=\"{$row['id_tag']} \">{$row['tag_name']}</option>";
+}
+
+$stmt->close();
+$conn->close();
+?>
