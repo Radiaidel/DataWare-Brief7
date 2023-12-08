e31839a2c64cb2ce8cd359e62936b8fa3bfba617
radiaide <idelkadiradi@gmail.com>
2023-12-08 22:58:36 +0100

    style de page community

--- Membre/src/action/community.php
+++ Membre/src/action/community.php
@@ -15,69 +15,60 @@ include("../../../includes/config/connection.php");
         integrity="sha384-GLhlTQ8iN17SJLlFfZVfP5z01K4JPTNqDQ5a6jgl5Up3H+9TP5IotK2+Obr4u" crossorigin="anonymous" />
 </head>
 
-<body class="bg-gray-200 ">
-    <!--header-->
+<body> <!--header-->
     <?php
     include '../../template/header.php';
     ?>
-    <div class="flex flex-col lg:flex-row">
-        <div class="w-full lg:w-2/4 pr-4 mb-4 lg:mb-0  ">
-            <div class="flex items-center space-x-3 mb-4 my-5">
-                <div class="max-w-md mx-auto">
-                    <div class="relative inline-block text-left">
-                        <div>
-                            <button type="button" id="filterBotton"
-                                class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                                Filter
-                                <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                    fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd"
-                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                            </button>
-                        </div>
+    <div class="w-full p-4 mb-4 lg:mb-0 bg-gray-200 rounded-md shadow-lg text-white">
 
-                        <div id="MenuFilter"
-                            class="hidden  origin-top-right absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                            <div class="py-1">
-                                <a href="#"
-                                    class="filter-option block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                                    data-filter-type="new">Date (New)</a>
-                                <a href="#"
-                                    class="filter-option block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                                    data-filter-type="old">Date (Old)</a>
-                                <a href="#"
-                                    class="filter-option block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                                    data-filter-type="all">All Questions</a>
-                                <a href="#"
-                                    class="filter-option block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                                    data-filter-type="my">My Questions</a>
-                            </div>
-                        </div>
+    <div class="mx-auto w-3/5 flex items-center space-x-3 mb-4 my-5 z-10   p-4">
+                    <div class="relative inline-block text-left">
+                <div>
+                    <button type="button" id="filterBotton"
+                        class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-gray-600 text-sm font-medium text-gray-300 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                        Filter
+                        <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                            fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd"
+                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
+
+                <div id="MenuFilter"
+                    class="hidden origin-top-right absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-gray-600 ring-1 ring-black ring-opacity-5">
+                    <div class="py-1">
+                        <a href="#"
+                            class="filter-option block px-4 py-2 text-sm text-gray-300 hover:bg-gray-500 hover:text-gray-100"
+                            data-filter-type="new">Date (New)</a>
+                        <a href="#"
+                            class="filter-option block px-4 py-2 text-sm text-gray-300 hover:bg-gray-500 hover:text-gray-100"
+                            data-filter-type="old">Date (Old)</a>
+                        <a href="#"
+                            class="filter-option block px-4 py-2 text-sm text-gray-300 hover:bg-gray-500 hover:text-gray-100"
+                            data-filter-type="all">All Questions</a>
+                        <a href="#"
+                            class="filter-option block px-4 py-2 text-sm text-gray-300 hover:bg-gray-500 hover:text-gray-100"
+                            data-filter-type="my">My Questions</a>
                     </div>
                 </div>
-
-                <div class="flex-1">
-                    <form id="searchForm" onsubmit="searchQuestions(); return false;" method="get">
-                        <input type="text" id="searchInput" placeholder="Search questions..."
-                            class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500">
-                    </form>
-                </div>
-
-            </div>
-            <div id="questionsContainer">
-
             </div>
 
+            <div class="flex-1">
+                <form id="searchForm" onsubmit="searchQuestions(); return false;" method="get">
+                    <input type="text" id="searchInput" placeholder="Search questions..."
+                        class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 bg-gray-600 text-white">
+                </form>
+            </div>
+        </div>
+
+        <div id="questionsContainer" class="mx-auto max-w-3/5">
+            <!-- Contenu des questions -->
         </div>
 
     </div>
 
-    <div class="w-full lg:w-3/4">
-        <!-- Content for the right side (question and answers) goes here -->
-    </div>
-    </div>
 
     <script>
 
--- Membre/src/action/load_questions.php
+++ Membre/src/action/load_questions.php
@@ -1,4 +1,5 @@
 <?php
+
 include("../../../includes/config/connection.php");
 session_start();
 
@@ -6,7 +7,7 @@ $page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
 $limit = 3;
 $offset = max(0, ($page - 1) * $limit);  // Ensure that offset is non-negative
 $sql = "";
-
+$userId = $_SESSION['id'];
 
 
 if (isset($_GET['filter'])) {
@@ -14,21 +15,21 @@ if (isset($_GET['filter'])) {
 
     switch ($filter) {
         case 'new':
-            $sql = "SELECT * FROM question ORDER BY created_at DESC";
+            $sql = "SELECT q.*, u.image_url, u.username FROM question q  INNER JOIN users u ON q.user_id = u.id_user ORDER BY created_at DESC ";
             break;
 
         case 'old':
-            $sql = "SELECT * FROM question ORDER BY created_at ASC";
+            $sql = "SELECT q.*, u.image_url, u.username FROM question q  INNER JOIN users u ON q.user_id = u.id_user  ORDER BY created_at ASC";
             break;
 
         case 'all':
-            $sql = "SELECT * FROM question";
+            $sql = "SELECT q.*, u.image_url, u.username FROM question q  INNER JOIN users u ON q.user_id = u.id_user ";
             break;
 
         case 'my':
             if (isset($_SESSION['id'])) {
                 $userId = $_SESSION['id'];
-                $sql = "SELECT * FROM question WHERE user_id = $userId";
+                $sql = "SELECT q.*, u.image_url, u.username FROM question q  INNER JOIN users u ON q.user_id = u.id_user  WHERE q.user_id = $userId";
             } else {
                 echo "User not logged in.";
                 exit;
@@ -43,7 +44,7 @@ if (isset($_GET['filter'])) {
             }
 
             $searchQuery = $_GET['search'];
-          
+
             $keywords = explode(" ", $searchQuery);
             $whereClause = "";
 
@@ -53,9 +54,10 @@ if (isset($_GET['filter'])) {
 
             $whereClause = rtrim($whereClause, " AND ");
 
-            $sql = "SELECT * FROM question LEFT JOIN question_tag ON question.question_id = question_tag.id_question
-                        LEFT JOIN tags ON question_tag.id_tag = tags.id_tag
-                        WHERE $whereClause";
+            $sql = "SELECT q.*, u.image_url, u.username FROM question q INNER JOIN users u ON q.user_id = u.id_user 
+            LEFT JOIN question_tag ON q.question_id = question_tag.id_question
+            LEFT JOIN tags ON question_tag.id_tag = tags.id_tag
+            WHERE $whereClause";
 
             break;
 
@@ -75,57 +77,100 @@ if (isset($_GET['filter'])) {
 
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            echo '<div class="max-w-xl bg-white p-8 rounded-md shadow-md my-5 cursor-pointer hover:shadow-xl">';
+            $imagePath = htmlspecialchars("/DataWare-Brief7/" . $row['image_url']);
+            $insertionDate = $row['created_at'];
+            $username = $row['username'];
+            $questionTitre = $row['title_question'];
+            $questionText = $row['question_text'];
+            $id_question = $row['question_id'];
 
-            echo '<div class="flex items-center text-gray-600 mb-4">';
-            echo '<div class="flex-shrink-0">';
-            echo '<svg width="30px" height="30px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#17d924" stroke="#17d924">';
-            // Your SVG content here
-            echo '</svg>';
-            echo '</div>';
-            echo '<div class="ml-2">';
-            echo '<p class="text-sm">Posted by ' . $row['user_id'] . '</p>';
-            echo '<p class="text-xs">Posted on ' . $row['created_at'] . '</p>';
-            echo '</div>';
-            echo '</div>';
+            $tagsSql = "SELECT t.tag_name FROM tags t JOIN question_tag qt ON qt.id_tag = t.id_tag WHERE qt.id_question = " . $id_question;
+            $tagsResult = $conn->query($tagsSql);
 
-            echo '<div class="mb-6">';
-            echo '<p class="text-gray-700">' . $row['question_text'] . '</p>';
-            echo '</div>';
 
-            echo '<div class="mb-4">';
-            echo '<span class="inline-block bg-blue-200 text-blue-800 py-1 px-2 rounded mr-2">#web</span>';
-            echo '<span class="inline-block bg-green-200 text-green-800 py-1 px-2 rounded mr-2">#product</span>';
-            echo '<span class="inline-block bg-yellow-200 text-yellow-800 py-1 px-2 rounded mr-2">#html</span>';
-            echo '<span class="inline-block bg-yellow-200 text-yellow-800 py-1 px-2 rounded mr-2">#js</span>';
-            echo '</div>';
 
-            echo '<div class="flex items-center space-x-4">';
+            ?>
+            <div class="mx-auto w-3/5  bg-gray-800 p-8 rounded-xl shadow-xl text-white mb-4">
+                <div class="flex items-center text-gray-300 mb-4">
+                    <div class="flex-shrink-0">
+                        <img src="<?php echo $imagePath; ?>" alt="User Image" class="w-10 h-10 rounded-full">
+                    </div>
+                    <div class="ml-2">
+                        <p class="text-sm">
+                            <?php echo $username; ?>
+                        </p>
+                        <p class="text-xs">
+                            <?php echo $insertionDate; ?>
+                        </p>
+                    </div>
+                </div>
 
-            echo '<button class="flex items-center text-gray-600 hover:text-blue-500">';
-            echo '<svg fill="#0473c8" width="20px" height="20px" viewBox="0 0 24 24" id="a11298b2-e15e-46f5-bfd2-69e168954b14" data-name="Livello 1" xmlns="http://www.w3.org/2000/svg" stroke="#0473c8">';
-            // Your SVG content here
-            echo '</svg>';
-            echo '<span class="ml-1">' . $row['likes'] . '</span>';
-            echo '</button>';
+                <div class="mb-6">
+                    <h1 class="text-white text-2xl font-bold">
+                        <?php echo $questionTitre; ?>
+                    </h1>
+                    <br>
+                    <p class="text-gray-300">
+                        <?php echo $questionText; ?>
+                    </p>
+                </div>
 
-            echo '<button class="flex items-center text-gray-600 hover:text-red-500">';
-            echo '<svg fill="#0473c8" height="20px" width="20px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" stroke="#0473c8">';
-            // Your SVG content here
-            echo '</svg>';
-            echo '<span class="ml-1">' . $row['dislikes'] . '</span>';
-            echo '</button>';
+                <div class="mb-4 space-x-2">
+                    <?php
+                    // Your existing code for displaying tags
+                    if ($tagsResult->num_rows > 0) {
+                        while ($tagRow = $tagsResult->fetch_assoc()) {
+                            ?>
+                            <span class="inline-block bg-blue-200 text-blue-800 py-1 px-2 rounded">
+                                <?php echo '#' . $tagRow['tag_name']; ?>
+                            </span>
+                            <?php
+                        }
+                    }
+                    ?>
+                </div>
 
-            echo '<button class="flex items-center text-gray-600 hover:text-green-500">';
-            echo '<svg width="20px" height="20px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">';
-            // Your SVG content here
-            echo '</svg>';
-            echo '<span class="ml-1">Respond</span>';
-            echo '</button>';
+                <div class="flex items-center space-x-4 mb-4"> <!-- Ajout de la classe mb-4 ici -->
+                    <button class="flex items-center text-gray-300 hover:text-blue-500">
 
-            echo '</div>';
+                       <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20.9752 12.1852L20.2361 12.0574L20.9752 12.1852ZM20.2696 16.265L19.5306 16.1371L20.2696 16.265ZM6.93777 20.4771L6.19056 20.5417L6.93777 20.4771ZM6.12561 11.0844L6.87282 11.0198L6.12561 11.0844ZM13.995 5.22142L14.7351 5.34269V5.34269L13.995 5.22142ZM13.3323 9.26598L14.0724 9.38725V9.38725L13.3323 9.26598ZM6.69814 9.67749L6.20855 9.10933H6.20855L6.69814 9.67749ZM8.13688 8.43769L8.62647 9.00585H8.62647L8.13688 8.43769ZM10.5181 4.78374L9.79208 4.59542L10.5181 4.78374ZM10.9938 2.94989L11.7197 3.13821V3.13821L10.9938 2.94989ZM12.6676 2.06435L12.4382 2.77841L12.4382 2.77841L12.6676 2.06435ZM12.8126 2.11093L13.042 1.39687L13.042 1.39687L12.8126 2.11093ZM9.86195 6.46262L10.5235 6.81599V6.81599L9.86195 6.46262ZM13.9047 3.24752L13.1787 3.43584V3.43584L13.9047 3.24752ZM11.6742 2.13239L11.3486 1.45675V1.45675L11.6742 2.13239ZM3.9716 21.4707L3.22439 21.5353L3.9716 21.4707ZM3 10.2342L3.74721 10.1696C3.71261 9.76945 3.36893 9.46758 2.96767 9.4849C2.5664 9.50221 2.25 9.83256 2.25 10.2342H3ZM20.2361 12.0574L19.5306 16.1371L21.0087 16.3928L21.7142 12.313L20.2361 12.0574ZM13.245 21.25H8.59635V22.75H13.245V21.25ZM7.68498 20.4125L6.87282 11.0198L5.3784 11.149L6.19056 20.5417L7.68498 20.4125ZM19.5306 16.1371C19.0238 19.0677 16.3813 21.25 13.245 21.25V22.75C17.0712 22.75 20.3708 20.081 21.0087 16.3928L19.5306 16.1371ZM13.2548 5.10015L12.5921 9.14472L14.0724 9.38725L14.7351 5.34269L13.2548 5.10015ZM7.18773 10.2456L8.62647 9.00585L7.64729 7.86954L6.20855 9.10933L7.18773 10.2456ZM11.244 4.97206L11.7197 3.13821L10.2678 2.76157L9.79208 4.59542L11.244 4.97206ZM12.4382 2.77841L12.5832 2.82498L13.042 1.39687L12.897 1.3503L12.4382 2.77841ZM10.5235 6.81599C10.8354 6.23198 11.0777 5.61339 11.244 4.97206L9.79208 4.59542C9.65573 5.12107 9.45699 5.62893 9.20042 6.10924L10.5235 6.81599ZM12.5832 2.82498C12.8896 2.92342 13.1072 3.16009 13.1787 3.43584L14.6307 3.05921C14.4252 2.26719 13.819 1.64648 13.042 1.39687L12.5832 2.82498ZM11.7197 3.13821C11.7548 3.0032 11.8523 2.87913 11.9998 2.80804L11.3486 1.45675C10.8166 1.71309 10.417 2.18627 10.2678 2.76157L11.7197 3.13821ZM11.9998 2.80804C12.1345 2.74311 12.2931 2.73181 12.4382 2.77841L12.897 1.3503C12.3873 1.18655 11.8312 1.2242 11.3486 1.45675L11.9998 2.80804ZM14.1537 10.9842H19.3348V9.4842H14.1537V10.9842ZM4.71881 21.4061L3.74721 10.1696L2.25279 10.2988L3.22439 21.5353L4.71881 21.4061ZM3.75 21.5127V10.2342H2.25V21.5127H3.75ZM3.22439 21.5353C3.2112 21.3828 3.33146 21.25 3.48671 21.25V22.75C4.21268 22.75 4.78122 22.1279 4.71881 21.4061L3.22439 21.5353ZM14.7351 5.34269C14.8596 4.58256 14.8241 3.80477 14.6307 3.0592L13.1787 3.43584C13.3197 3.97923 13.3456 4.54613 13.2548 5.10016L14.7351 5.34269ZM8.59635 21.25C8.12244 21.25 7.72601 20.887 7.68498 20.4125L6.19056 20.5417C6.29852 21.7902 7.3427 22.75 8.59635 22.75V21.25ZM8.62647 9.00585C9.30632 8.42 10.0392 7.72267 10.5235 6.81599L9.20042 6.10924C8.85404 6.75767 8.3025 7.30493 7.64729 7.86954L8.62647 9.00585ZM21.7142 12.313C21.9695 10.8365 20.8341 9.4842 19.3348 9.4842V10.9842C19.9014 10.9842 20.3332 11.4959 20.2361 12.0574L21.7142 12.313ZM3.48671 21.25C3.63292 21.25 3.75 21.3684 3.75 21.5127H2.25C2.25 22.1953 2.80289 22.75 3.48671 22.75V21.25ZM12.5921 9.14471C12.4344 10.1076 13.1766 10.9842 14.1537 10.9842V9.4842C14.1038 9.4842 14.0639 9.43901 14.0724 9.38725L12.5921 9.14471ZM6.87282 11.0198C6.8474 10.7258 6.96475 10.4378 7.18773 10.2456L6.20855 9.10933C5.62022 9.61631 5.31149 10.3753 5.3784 11.149L6.87282 11.0198Z" fill="#e6e6e6"></path> </g></svg>
+                        <span class="ml-1">
+                            <?php echo $row['likes']; ?>
+                        </span>
+                    </button>
 
-            echo '</div>';
+                    <button class="flex items-center text-gray-300 hover:text-red-500">
+                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(180)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20.9752 12.1852L20.2361 12.0574L20.9752 12.1852ZM20.2696 16.265L19.5306 16.1371L20.2696 16.265ZM6.93777 20.4771L6.19056 20.5417L6.93777 20.4771ZM6.12561 11.0844L6.87282 11.0198L6.12561 11.0844ZM13.995 5.22142L14.7351 5.34269V5.34269L13.995 5.22142ZM13.3323 9.26598L14.0724 9.38725V9.38725L13.3323 9.26598ZM6.69814 9.67749L6.20855 9.10933H6.20855L6.69814 9.67749ZM8.13688 8.43769L8.62647 9.00585H8.62647L8.13688 8.43769ZM10.5181 4.78374L9.79208 4.59542L10.5181 4.78374ZM10.9938 2.94989L11.7197 3.13821V3.13821L10.9938 2.94989ZM12.6676 2.06435L12.4382 2.77841L12.4382 2.77841L12.6676 2.06435ZM12.8126 2.11093L13.042 1.39687L13.042 1.39687L12.8126 2.11093ZM9.86195 6.46262L10.5235 6.81599V6.81599L9.86195 6.46262ZM13.9047 3.24752L13.1787 3.43584V3.43584L13.9047 3.24752ZM11.6742 2.13239L11.3486 1.45675V1.45675L11.6742 2.13239ZM3.9716 21.4707L3.22439 21.5353L3.9716 21.4707ZM3 10.2342L3.74721 10.1696C3.71261 9.76945 3.36893 9.46758 2.96767 9.4849C2.5664 9.50221 2.25 9.83256 2.25 10.2342H3ZM20.2361 12.0574L19.5306 16.1371L21.0087 16.3928L21.7142 12.313L20.2361 12.0574ZM13.245 21.25H8.59635V22.75H13.245V21.25ZM7.68498 20.4125L6.87282 11.0198L5.3784 11.149L6.19056 20.5417L7.68498 20.4125ZM19.5306 16.1371C19.0238 19.0677 16.3813 21.25 13.245 21.25V22.75C17.0712 22.75 20.3708 20.081 21.0087 16.3928L19.5306 16.1371ZM13.2548 5.10015L12.5921 9.14472L14.0724 9.38725L14.7351 5.34269L13.2548 5.10015ZM7.18773 10.2456L8.62647 9.00585L7.64729 7.86954L6.20855 9.10933L7.18773 10.2456ZM11.244 4.97206L11.7197 3.13821L10.2678 2.76157L9.79208 4.59542L11.244 4.97206ZM12.4382 2.77841L12.5832 2.82498L13.042 1.39687L12.897 1.3503L12.4382 2.77841ZM10.5235 6.81599C10.8354 6.23198 11.0777 5.61339 11.244 4.97206L9.79208 4.59542C9.65573 5.12107 9.45699 5.62893 9.20042 6.10924L10.5235 6.81599ZM12.5832 2.82498C12.8896 2.92342 13.1072 3.16009 13.1787 3.43584L14.6307 3.05921C14.4252 2.26719 13.819 1.64648 13.042 1.39687L12.5832 2.82498ZM11.7197 3.13821C11.7548 3.0032 11.8523 2.87913 11.9998 2.80804L11.3486 1.45675C10.8166 1.71309 10.417 2.18627 10.2678 2.76157L11.7197 3.13821ZM11.9998 2.80804C12.1345 2.74311 12.2931 2.73181 12.4382 2.77841L12.897 1.3503C12.3873 1.18655 11.8312 1.2242 11.3486 1.45675L11.9998 2.80804ZM14.1537 10.9842H19.3348V9.4842H14.1537V10.9842ZM4.71881 21.4061L3.74721 10.1696L2.25279 10.2988L3.22439 21.5353L4.71881 21.4061ZM3.75 21.5127V10.2342H2.25V21.5127H3.75ZM3.22439 21.5353C3.2112 21.3828 3.33146 21.25 3.48671 21.25V22.75C4.21268 22.75 4.78122 22.1279 4.71881 21.4061L3.22439 21.5353ZM14.7351 5.34269C14.8596 4.58256 14.8241 3.80477 14.6307 3.0592L13.1787 3.43584C13.3197 3.97923 13.3456 4.54613 13.2548 5.10016L14.7351 5.34269ZM8.59635 21.25C8.12244 21.25 7.72601 20.887 7.68498 20.4125L6.19056 20.5417C6.29852 21.7902 7.3427 22.75 8.59635 22.75V21.25ZM8.62647 9.00585C9.30632 8.42 10.0392 7.72267 10.5235 6.81599L9.20042 6.10924C8.85404 6.75767 8.3025 7.30493 7.64729 7.86954L8.62647 9.00585ZM21.7142 12.313C21.9695 10.8365 20.8341 9.4842 19.3348 9.4842V10.9842C19.9014 10.9842 20.3332 11.4959 20.2361 12.0574L21.7142 12.313ZM3.48671 21.25C3.63292 21.25 3.75 21.3684 3.75 21.5127H2.25C2.25 22.1953 2.80289 22.75 3.48671 22.75V21.25ZM12.5921 9.14471C12.4344 10.1076 13.1766 10.9842 14.1537 10.9842V9.4842C14.1038 9.4842 14.0639 9.43901 14.0724 9.38725L12.5921 9.14471ZM6.87282 11.0198C6.8474 10.7258 6.96475 10.4378 7.18773 10.2456L6.20855 9.10933C5.62022 9.61631 5.31149 10.3753 5.3784 11.149L6.87282 11.0198Z" fill="#e6e6e6"></path> </g></svg>
+                        <span class="ml-1">
+                            <?php echo $row['dislikes']; ?>
+                        </span>
+                    </button>
+                </div>
+
+                <ul role="list" class="flex justify-center space-x-5">
+                    <?php
+                    // Check if the logged-in user is the creator of the question
+                    if ($userId == $row['user_id']) {
+                        ?>
+                        <li>
+                            <a href="modifierquestion.php?modifierID=<?php echo $id_question; ?>"
+                                class="text-indigo-300 hover:text-indigo-500">
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z" stroke="#e5e5e5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13" stroke="#e5e5e5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="deletequestion.php?DeleteID=<?php echo $id_question; ?>"
+                                class="text-indigo-300 hover:text-indigo-500">
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M10 12V17" stroke="#e3e3e3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M14 12V17" stroke="#e3e3e3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M4 7H20" stroke="#e3e3e3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 10V18C6 19.6569 7.34315 21 9 21H15C16.6569 21 18 19.6569 18 18V10" stroke="#e3e3e3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#e3e3e3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                            </a>
+                        </li>
+                        <?php
+                    }
+                    ?>
+                </ul>
+            </div>
+
+            <?php
         }
     } else {
         echo "Aucune question trouvée.";
@@ -155,9 +200,8 @@ if (isset($_GET['filter'])) {
     <div class="flex items-center justify-center mt-4">
         <?php
         for ($i = 1; $i <= $totalPages; $i++) {
-            echo '<button class="bg-blue-500 text-white px-4 py-2 rounded-full mr-2" onclick="DisplayQuestions(\'' . $filter . '\',' . $i . ')">' . $i . '</button>';
-        }
-        ?>
+            echo '<button class="flex-shrink-0 w-10 h-10 bg-gray-800 border border-white rounded-full mr-2 focus:outline-none hover:bg-gray-900" onclick="DisplayQuestions(\'' . $filter . '\',' . $i . ')">' . $i . '</button>';
+        } ?>
     </div>
     <script src="../../../Javascript/pagination.js" defer></script>
     <?php
