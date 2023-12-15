9195844f5fec4c1f58586ce9acdbd96aa22e2927
ER-RADA Oumaima <erradaoumaima@gmail.com>
2023-12-16 00:01:42 +0100

    po_change

--- ProductOwner/src/action/delete_project.php
+++ ProductOwner/src/action/delete_project.php
@@ -1,14 +1,14 @@
 <?php
 // Include your database connection file
-require_once "/xampp/htdocs/data/includes/config.php";
+require_once "../../../includes/config/connection.php"; 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["project_id"])) {
     // Get the project ID from the POST request
     $projectId = $_POST["project_id"];
 
     // Delete the project from the database
-    $query = "DELETE FROM projects WHERE id = ?";
-    $stmt = $con->prepare($query);
+    $query = "DELETE FROM project WHERE Id_Project= ?";
+    $stmt = $conn->prepare($query);
 
     if ($stmt) {
         // Bind the project ID parameter
@@ -19,15 +19,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["project_id"])) {
         $stmt->close();
 
         // Redirect to the projects page or show a success message
-        header("Location: /data/product_owner/src/index.php");
+        header("Location: ./index.php");
         exit();
     } else {
         // Handle the error
-        echo "Error: " . $con->error;
+        echo "Error: " . $conn->error;
     }
 }
 
 // Close the database connection
-$con->close();
+$conn->close();
 ?>
 
